4714a1841c764114825fb7f4e41467d570a0eef2
ifat <i>
2024-08-07 21:44:18 +0100

    nested route added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <h2>NavBar</h2>
+      <nav>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
